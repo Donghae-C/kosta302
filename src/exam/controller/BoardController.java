d6586cca7241a90d945dcf52c2a2762a32472f79
donghae <160084378+Donghae-C@users.noreply.github.com>
2025-08-28 15:35:29 +0900

    Update BoardController.java

--- src/exam/controller/BoardController.java
+++ src/exam/controller/BoardController.java
@@ -4,8 +4,11 @@ public class BoardController {
 
 	public void select() {
 		System.out.println("로컬 작업");
-
+	}
 	public void insert() {
 		System.out.println("서버 작업");
-	
+	}
+	public void delete() {
+		System.out.println("서버에서 delete 1");
+	}
 }
