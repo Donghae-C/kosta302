73e8834f16f7ebcfbc76cc4d56135452d24cb96f
donghae <choi7gold@gmail.com>
2025-08-28 15:43:15 +0900

    merge 해놨음

--- src/exam/controller/BoardController.java
+++ src/exam/controller/BoardController.java
@@ -5,10 +5,16 @@ public class BoardController {
 	public void select() {
 		System.out.println("로컬 작업");
 	}
+
 	public void insert() {
 		System.out.println("서버 작업");
 	}
+
 	public void delete() {
 		System.out.println("서버에서 delete 1");
+	}
+
+	public void update() {
+		System.out.println("로컬에서 update 1");
 	}
 }
